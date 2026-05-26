789e914d849fc8a5652ee0022d06a420bad3bf5d
kyoka09ahad <abdulahad09rv1365@gmail.com>
2026-05-26 17:53:04 +0530

    Update initialise.c

---
+++ Sample_Folder/initialise.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("Welcome to the C programming language!\n");
+
+    int a,b;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    
+    printf("You entered: %d and %d\n", a, b);
+
+    printf("Update: In this sample file I am learning how to use git and github for version control and collaboration.\n");
+    return 0;
+}
