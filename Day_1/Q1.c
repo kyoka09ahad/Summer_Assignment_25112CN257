cc3215b08167ef5a018980be82d727f80bd81730
kyoka09ahad <abdulahad09rv1365@gmail.com>
2026-05-28 10:11:35 +0530

    Create Q1.c

---
+++ Day_1/Q1.c
@@ -0,0 +1,23 @@
+//Write a program to calculate sum of first N natural numbers.
+
+#include <stdio.h>
+
+int main()
+{
+    long long n, sum ;
+
+    printf("Enter the Natural Number upto which the sum has to be calculated: \n");
+    scanf("%lld", &n);
+
+    if(n <= 0)
+    {
+        printf("Error: Please enter only 'Natural Numbers'.\n");
+        return 1;
+    }
+
+    sum = n*(n+1)/2;
+
+    printf("Thus, the sum of first %lld Natural Numbers is: %lld\n", n, sum);
+
+    return 0 ;
+}
