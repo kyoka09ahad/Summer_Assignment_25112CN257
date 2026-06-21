eeabb22ccc658be56090df527fa22b454be45983
kyoka09ahad <abdulahad09rv1365@gmail.com>
2026-06-21 23:59:53 +0530

    Update Q84.c

--- Day_21/Q84.c
+++ Day_21/Q84.c
@@ -1,1 +1,28 @@
 //Write a program to convert lowercase to uppercase.
+
+#include <stdio.h>
+
+int main()
+{
+    char str[1000];
+
+    printf("Enter a string: ");
+
+    if (fgets(str, sizeof(str), stdin) == NULL)
+    {
+        printf("Input error.\n");
+        return 1;
+    }
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - ('a' - 'A');
+        }
+    }
+
+    printf("Uppercase string: %s", str);
+
+    return 0;
+}
